494ac078fa6c5948a30ea5c87392bc56109d21a6
Albert <albert@rss3.io>
2022-12-21 19:00:10 +0800

    feat: add source code

---
+++ contracts/chain/sidechain/CrossbellWETH.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.17;
+
+import "../../references/ERC20/ERC20Detailed.sol";
+import "../../references/ERC20/ERC20Mintable.sol";
+
+contract CrossbellWETH is ERC20Detailed, ERC20Mintable {
+    constructor() public ERC20Detailed("Crossbell Wrapped Ether", "WETH", 18) {}
+}

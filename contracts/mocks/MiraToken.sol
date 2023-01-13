8b5569ff2ad89567a34567e5a17d6d5e499cf545
Albert <albert@rss3.io>
2023-01-13 16:43:08 +0800

    chore: add erc20 token

---
+++ contracts/mocks/MiraToken.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MiraToken is Context, AccessControlEnumerable, ERC20 {
+    bytes32 public constant BLOCK_ROLE = keccak256("BLOCK_ROLE");
+
+    uint8 internal _decimals;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
+        // Grant `DEFAULT_ADMIN_ROLE` to the account that deploys the contract
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _decimals = decimals_;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev Create `amount` new tokens for `to`.
+     * Requirements:
+     * - the caller must have the `DEFAULT_ADMIN_ROLE`.
+     */
+    function mint(address to, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Block transfers tokens from account `from` who has the `BLOCK_ROLE`.
+     */
+    function _transfer(address from, address to, uint256 amount) internal override {
+        require(!hasRole(BLOCK_ROLE, from), "transfer is blocked");
+        super._transfer(from, to, amount);
+    }
+}

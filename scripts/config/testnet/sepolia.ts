3e4f1afaef867d68c8f0096feb0c189c7ffba61f
Albert <albert@rss3.io>
2023-01-28 11:02:56 +0800

    chore: update slither

--- scripts/config/testnet/sepolia.ts
+++ scripts/config/testnet/sepolia.ts
@@ -2,9 +2,9 @@ export const proxyAdmin = "0xc72cE0090718502f08506c4592F18f13094d4CE3";
 export const validatorContract = "0xbf58a5d64F451f537ABdB8B0203eF3F105097285";
 export const gatewayAdmin = "0x678e0E67555E8fC4533c1a9f204e2C1C7C1C9665";
 export const crossbellTokens = [
-    "0x73bE5E9f82f45564565Ffb53F52b23eAB32032F9",
-    "0x73bE5E9f82f45564565Ffb53F52b23eAB32032F9",
-    "0x73bE5E9f82f45564565Ffb53F52b23eAB32032F9",
+    "0x2a901D3Bec0c9436c6027B4935aD3963d8a954Ed",
+    "0x2a901D3Bec0c9436c6027B4935aD3963d8a954Ed",
+    "0x2a901D3Bec0c9436c6027B4935aD3963d8a954Ed",
 ];
 export const chainIds = [5, 80001, 97]; // goerli, polygonMumbai, bscTestnt
 export const mainchainTokens = [

f1662b2a01cc38f80cfab7d17e9d3f6264cb8e32
Albert <albert@rss3.io>
2023-01-31 16:12:19 +0800

    feat: update upgradeable checker

--- scripts/checkUpgradeable.sh
+++ scripts/checkUpgradeable.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 #set -x
 
+# install slither
+which slither-check-upgradeability
+if [ $? -ne 0 ]; then
+  git clone https://github.com/crytic/slither.git && cd slither || exit 1
+  pip3 install .
+  cd ..
+fi
+
+# copy .env file
+if [ ! -f ".env" ]; then
+  cp ".env.example" ".env"
+fi
+
 # create tmp files
-file1=$(mktemp /tmp/crossbell-bridge-slither-check.XXXXX) || exit 1
-file2=$(mktemp /tmp/crossbell-bridge-slither-check.XXXXX) || exit 1
+file1=$(mktemp /tmp/crossbell-bridge-slither-check.XXXXX) || exit 2
+file2=$(mktemp /tmp/crossbell-bridge-slither-check.XXXXX) || exit 2
 
 # slither-check
 echo "MainchainGateway: " >"$file1"
@@ -28,8 +41,14 @@ lines2=$(sed -n '$=' "$file2")
 # if the check fails, there will be 2+ lines in the files
 if [ "$lines1" -gt 2 ] || [ "$lines2" -gt 2 ]
 then
-  echo "upgradeable check failed"
+  echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+  echo "slither-check failed"
   cat "$file1"
   cat "$file2"
-  exit 2
+  echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+  exit 255
+else
+  echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+  echo "slither-check done"
+  echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 fi

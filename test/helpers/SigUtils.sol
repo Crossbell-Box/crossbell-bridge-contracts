d0cc9b0c096d49361189f8f99de9f669e5e2cd43
Albert <albert@rss3.io>
2023-02-03 16:49:38 +0800

    feat: fix solhint

--- test/helpers/SigUtils.sol
+++ test/helpers/SigUtils.sol
@@ -1,17 +1,8 @@
 // SPDX-License-Identifier: MIT
+// solhint-disable comprehensive-interface
 pragma solidity 0.8.16;
 
 contract SigUtils {
-    bytes32 internal DOMAIN_SEPARATOR;
-
-    constructor(bytes32 _DOMAIN_SEPARATOR) {
-        DOMAIN_SEPARATOR = _DOMAIN_SEPARATOR;
-    }
-
-    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 public constant PERMIT_TYPEHASH =
-        0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-
     struct Permit {
         address owner;
         address spender;
@@ -20,23 +11,34 @@ contract SigUtils {
         uint256 deadline;
     }
 
+    // solhint-disable-next-line var-name-mixedcase,private-vars-leading-underscore
+    bytes32 internal DOMAIN_SEPARATOR;
+
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH =
+        0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    constructor(bytes32 domainSeparator) {
+        DOMAIN_SEPARATOR = domainSeparator;
+    }
+
+    // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
+    function getTypedDataHash(Permit memory permit) public view returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, _getStructHash(permit)));
+    }
+
     // computes the hash of a permit
-    function getStructHash(Permit memory _permit) internal pure returns (bytes32) {
+    function _getStructHash(Permit memory permit) internal pure returns (bytes32) {
         return
             keccak256(
                 abi.encode(
                     PERMIT_TYPEHASH,
-                    _permit.owner,
-                    _permit.spender,
-                    _permit.value,
-                    _permit.nonce,
-                    _permit.deadline
+                    permit.owner,
+                    permit.spender,
+                    permit.value,
+                    permit.nonce,
+                    permit.deadline
                 )
             );
-    }
-
-    // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
-    function getTypedDataHash(Permit memory _permit) public view returns (bytes32) {
-        return keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, getStructHash(_permit)));
     }
 }

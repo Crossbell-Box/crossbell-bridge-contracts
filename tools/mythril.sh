60c7d81df9804e55bb16c2c2c14a425553786330
Albert <albert@rss3.io>
2023-02-07 12:01:58 +0800

    feat: update mythril checker

--- tools/mythril.sh
+++ tools/mythril.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 
 if [ ! -d "contracts" ]; then
 	echo "error: script needs to be run from project root './tools/mythril.sh'"
@@ -7,7 +7,14 @@ if [ ! -d "contracts" ]; then
 fi
 
 echo '
-myth analyze contracts/MainchainGateway.sol --solc-json mythril.config.json --max-depth 10 --execution-timeout 900  --solver-timeout 900 &&
-myth analyze contracts/CrossbellGateway.sol --solc-json mythril.config.json --max-depth 10 --execution-timeout 900  --solver-timeout 900 &&
-myth analyze contracts/Validator.sol --solc-json mythril.config.json --max-depth 10 --execution-timeout 900  --solver-timeout 900 ' |
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "MainchainGateway: "
+myth analyze contracts/MainchainGateway.sol --solc-json mythril.config.json --solv 0.8.16 --max-depth 10 --execution-timeout 900  --solver-timeout 900 &&
+echo "CrossbellGateway: "
+myth analyze contracts/CrossbellGateway.sol --solc-json mythril.config.json --solv 0.8.16 --max-depth 10 --execution-timeout 900  --solver-timeout 900 &&
+echo "Validator: "
+myth analyze contracts/Validator.sol --solc-json mythril.config.json --solv 0.8.16 --max-depth 10 --execution-timeout 900  --solver-timeout 900 &&
+echo "MiraToken: "
+myth analyze contracts/token/MiraToken.sol --solc-json mythril.config.json --solv 0.8.16 --max-depth 10 --execution-timeout 900  --solver-timeout 900 &&
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" ' |
 docker run --rm -v "$PWD":/project -i --workdir=/project --entrypoint=sh mythril/myth
